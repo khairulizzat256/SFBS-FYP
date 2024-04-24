5a2da743fce4ea8e041c68354c7622983319e4f8
Khairul Izzat <khairulizzat@graduate.utm.my>
2024-04-24 16:44:02 +0800

    Project Initialized

---
+++ sfbs_fyp/src/main/java/com/fyp/sfbs_fyp/SfbsFypApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.sfbs_fyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfbsFypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfbsFypApplication.class, args);
+	}
+
+}
---
+++ sfbs_fyp/src/test/java/com/fyp/sfbs_fyp/SfbsFypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.sfbs_fyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfbsFypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
